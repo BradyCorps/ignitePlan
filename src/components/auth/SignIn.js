eca25322e7397e865fb243059e34ffd9b344339b
Brady Corps <brady@bradycorps.ca>
2020-10-01 13:10:20 -0500

    v3 css styling

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -17,21 +17,25 @@ class SignIn extends Component {
 	};
 	render() {
 		return (
-			<div className="container">
-				<form onSubmit={this.handleSubmit} className="white">
-					<h5 className="grey-text text-darken-3">Sign-Up</h5>
+			<div className="container login">
+				<form onSubmit={this.handleSubmit}>
+					<h3 className="center-align white-text text-darken-3">Sign-In</h3>
 					<div className="input-field">
-						<label htmlFor="email">Email</label>
+						<label id="white" htmlFor="email">
+							Email
+						</label>
 						<input type="email" id="email" onChange={this.handleChange} />
 					</div>
 
 					<div className="input-field">
-						<label htmlFor="password">Password</label>
+						<label id="white" htmlFor="password">
+							Password
+						</label>
 						<input type="password" id="password" onChange={this.handleChange} />
 					</div>
 
-					<div className="input-field">
-						<button className="btn pink lighten-1 z-depth-0">Login</button>
+					<div className="center-align">
+						<button className="btn btn-large pink lighten-1 ">Login</button>
 					</div>
 				</form>
 			</div>
