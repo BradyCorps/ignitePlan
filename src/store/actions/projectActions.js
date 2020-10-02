0d053037d81cd309b0f30efca4b73583e3f586f5
Brady Corps <brady@bradycorps.ca>
2020-10-02 11:53:57 -0500

    FireBase connected w/ redux. Reducers & error catch

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,25 @@
 export const createProject = project => {
-	return (dispatch, getState) => {
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
 		// make async call to database
-		dispatch({ type: 'CREATE_PROJECT', project });
+		const firestore = getFirestore();
+		// async code for adding project to database
+		firestore
+			.collection('projects')
+			.add({
+				// spread operator same as using project.title && project.content
+				...project,
+				authorFireName: 'Brady',
+				authorLastName: 'Corps',
+				authorId: 12345,
+				createAt: new Date(),
+				// then method fires when action is complete --> doc added to collection
+			})
+			.then(() => {
+				dispatch({ type: 'CREATE_PROJECT', project });
+				// Catching error --> setting different dispatch for error
+			})
+			.catch(err => {
+				dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+			});
 	};
 };
